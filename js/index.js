36b1a3f71df17ec0b10b24632b8dc716f3a0c746
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-04 10:14:29 +0530

    use enter to run cmmds; bye-bye button
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- js/index.js
+++ js/index.js
@@ -4,24 +4,28 @@ $(document).ready(function () {
   console.log(`We are using node ${process.versions.node}, 
                Chrome ${process.versions.chrome}, and Electron ${process.versions.electron}.`)
 
-  const btn = $('#send');
   const inp = $('#input');
   const main = $('#mainarea');
 
   ipcRenderer.send('initial-ping', 'initial');
 
   ipcRenderer.on('initial-reply', (event, arg) => {
-    main.append(arg + "> ");
+    main.append("<span style='color: greenyellow'>" + arg + " $ </span>");
   })
 
-  btn.click(() => {
-    main.css('color', 'green').append(inp.val() + "<br>");
-    ipcRenderer.send('decode-cmd', inp.val())
-  })
+  inp.keypress(function (e) {
+    var key = e.which;
+    if(key == 13)  // the enter key code
+    {
+      main.append("<span style='color: white'>" + inp.val() + "<br>");
+      ipcRenderer.send('decode-cmd', inp.val())
+    }
+  });
 
   ipcRenderer.on('decode-cmd-reply', (event, arg) => {
+    inp.val('');
     console.log("received");
-    main.css('color', 'red').append(arg);
+    main.append("<span style='color: #66D9EF'>" + arg);
     console.log(arg);
     ipcRenderer.send('initial-ping', 'initial');
   })
